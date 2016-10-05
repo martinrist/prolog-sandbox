7674bb21be2c0c5c52e1a6cdd24b6e50a80d2e55
Martin Rist <mlr-github@martinrist.com>
2016-10-05 14:15:44 +0100

    Add day 1 exercise files

---
+++ library.pl
@@ -0,0 +1,13 @@
+% Library
+
+author(lordOfTheRings, tolkien).
+author(hobbit, tolkien).
+author(silmarillion, tolkien).
+
+author(anathem, stephenson).
+author(seveneves, stephenson).
+author(quicksilver, stephenson).
+
+author(neuromancer, gibson).
+author(burningChrome, gibson).
+
---
+++ musicians.pl
@@ -0,0 +1,10 @@
+plays(dylan, guitar).
+plays(dylan, harmonica).
+plays(dylan, vocals).
+
+plays(hendrix, guitar).
+plays(hendrix, vocals).
+
+genre(dylan, folk).
+genre(hendrix, rock).
+genre(hendrix, blues).
